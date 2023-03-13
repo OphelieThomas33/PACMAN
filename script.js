8d76f7b478a2489f35f0293dc293e32146c495af
OphelieThomas33 <ophelie_thomas@hotmail.com>
2023-03-13 14:15:52 +0100

    creation of the board and setting up travel

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const gameDiv = document.getElementById("game")
+const sizeCaseWidth = 28
 
 /* 
 * Créer le plateau (dynamique)
@@ -10,3 +11,122 @@ const gameDiv = document.getElementById("game")
 * 
 
 */
+
+const layout = [
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,3,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,3,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,4,4,4,4,4,4,4,4,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,2,2,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+    4,4,4,4,4,4,0,0,0,4,1,2,2,2,2,2,2,1,4,0,0,0,4,4,4,4,4,4,
+    1,1,1,1,1,1,0,1,1,4,1,2,2,2,2,2,2,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,1,1,1,1,1,0,1,1,4,1,1,1,1,1,1,1,1,4,1,1,0,1,1,1,1,1,1,
+    1,0,0,0,0,0,0,0,0,4,4,4,4,4,4,4,4,4,4,0,0,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,0,1,1,1,1,0,1,1,1,1,1,0,1,1,0,1,1,1,1,1,0,1,1,1,1,0,1,
+    1,3,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,3,1,
+    1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+    1,1,1,0,1,1,0,1,1,0,1,1,1,1,1,1,1,1,0,1,1,0,1,1,0,1,1,1,
+    1,0,0,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,1,1,0,0,0,0,0,0,1,
+    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+    1,0,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,1,1,1,1,1,1,1,1,1,0,1,
+    1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,
+    1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
+  ]
+  // 0 - pac-dots
+  // 1 - wall
+  // 2 - ghost-lair
+  // 3 - power-pellet
+  // 4 - empty
+creerPlateau()
+
+document.addEventListener("keyup", (event) =>{
+    DeplacerPacman(event.key);
+});
+
+function creerPlateau() {
+    let cptCase = 0
+    layout.forEach(caseLayout => {
+        let casePlateau = document.createElement('div');
+        casePlateau.dataset.numerocase = cptCase;
+        switch(caseLayout) {
+            case 0: 
+                casePlateau.classList.add('point');
+                break;
+            case 1: 
+                casePlateau.classList.add('mur');
+                break;
+            case 2: 
+                casePlateau.classList.add('fantome-area');
+                break;
+            case 3: 
+                casePlateau.classList.add('point-puissance');
+                break;
+            case 4: 
+                
+                break;
+
+        }
+        gameDiv.appendChild(casePlateau);
+        cptCase++;
+    })
+    getCaseByIndex(489).classList.add('pacman')
+}
+
+
+function getCaseByIndex(index) {
+    let caseGame = document.querySelector("[data-numerocase='"+index+"']")
+    return caseGame
+}
+
+function DeplacerPacman(direction){
+    let pacmanDiv = document.querySelector(".pacman");
+    let pacManCase = pacmanDiv.dataset.numerocase;
+    let caseDestination = null;
+    switch(direction){
+        case "ArrowUp" :
+            //Déplacer la case contenant pacman de 1 vers le haut
+            caseDestination =  getCaseByIndex(parseInt(pacManCase) - sizeCaseWidth);
+            
+        break;
+        case "ArrowRight" :
+            //Déplacer la case contenant pacman de 1 vers la droite
+            caseDestination =  getCaseByIndex(parseInt(pacManCase) + 1);
+        break;
+        case "ArrowLeft" :
+            //Déplacer la case contenant pacman de 1 vers la gauche
+            caseDestination =  getCaseByIndex(parseInt(pacManCase) - 1);
+        break;
+        case "ArrowDown" :
+            caseDestination =  getCaseByIndex(parseInt(pacManCase) + sizeCaseWidth);
+        default : 
+            break;
+    };
+    if(caseDestination != null){
+        if(checkDirection(caseDestination)){
+            pacmanDiv.classList.remove("pacman");
+            caseDestination.classList.add("pacman");
+        }
+    }
+}
+
+//return faux si je peux pas aller la où je veux
+//return vrai si je peux
+function checkDirection(caseDestination)
+{
+    if(caseDestination.classList.contains("mur")){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
