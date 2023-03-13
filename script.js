e622d8c97248ad731dfb2ce0da3eade8b053cec2
OphelieThomas33 <ophelie_thomas@hotmail.com>
2023-03-13 13:09:46 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const gameDiv = document.getElementById("game")
+
+/* 
+* Créer le plateau (dynamique)
+* Créer notre PACMAN
+* Gérer ses déplacements
+* Contraintes de déplacements (pas dans les murs)
+* Pièces à manger
+* Gérer les fantômes
+* 
+
+*/
